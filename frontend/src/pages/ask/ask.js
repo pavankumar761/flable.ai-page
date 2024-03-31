a584ebf5b5839d686caf7bcd243c60addf4bd91e
Pavan Kumar Routhu <pavankumar0167@Pavans-MacBook-Air.local>
2024-04-01 02:42:35 +0530

    updated reaction

--- frontend/src/pages/ask/ask.js
+++ frontend/src/pages/ask/ask.js
@@ -10,7 +10,6 @@ export default function Ask() {
 
     const handleSubmit= (e)=>{
       e.preventDefault();
-
     }
 
   return (
--- frontend/src/pages/reaction/reaction.js
+++ frontend/src/pages/reaction/reaction.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Reaction() {
+
+    const [likeState, likeStateFunc] = useState(false)
+    const [unLikeState, unLikeStateFunc] = useState(false)
+
+    const likeClick = ()=>{
+        likeStateFunc(!likeState)
+    }
+
+    const unLikeClick = () => {
+        unLikeStateFunc(!unLikeState)
+    }
+
   return (
     <div>
-        
+          <img onClick={likeClick} src={likeState ? like : nolike} />
+          <img onClick={unLikeClick} src={unLikeState ? unlike : nounlike} />
     </div>
   )
 }
